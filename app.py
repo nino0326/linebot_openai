73172d924ad4b6dd8c0598827bc015fdbfd2e6ff
nino0326 <80765385+nino0326@users.noreply.github.com>
2024-03-10 03:12:41 +0800

    Claude app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 import requests
 
